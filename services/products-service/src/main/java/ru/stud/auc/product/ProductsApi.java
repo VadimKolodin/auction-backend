e9c837267b67339cfeaa8a6072c33e28e671ebc5
Вадим Колодин <lefrere123@gmail.com>
2023-03-09 00:22:42 +0400

    AUC-6 Добавил создание товара (не тестил, нужны миграции)

---
+++ services/products-service/src/main/java/ru/stud/auc/product/ProductsApi.java
@@ -0,0 +1,29 @@
+package ru.stud.auc.product;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.stud.auc.flowdata.product.model.ProductView;
+import ru.stud.auc.product.model.ProductDto;
+
+import javax.validation.Valid;
+
+import static ru.stud.auc.product.ProductsApi.PRODUCTS_API_PATH;
+
+//TODO: методами может пользоваться только админ
+@Validated
+@RequestMapping(PRODUCTS_API_PATH)
+@Tag(name = "Методы для работы с ОДО", description = PRODUCTS_API_PATH)
+public interface ProductsApi {
+    String PRODUCTS_API_PATH = "/api/product";
+
+    @PostMapping
+    @Operation(summary = "Создание товара")
+    ProductView create(@Valid @RequestBody ProductDto request);
+
+}
--- services/products-service/src/main/java/ru/stud/auc/product/model/ProductDto.java
+++ services/products-service/src/main/java/ru/stud/auc/product/model/ProductDto.java
@@ -6,21 +6,28 @@ import lombok.NoArgsConstructor;
 import ru.stud.auc.common.enums.SubTag;
 import ru.stud.auc.common.enums.Tag;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
 
+    @NotBlank
+    @Pattern(regexp = "[^?!.,\\\\\"/';:&%$#@^*()-_]+")
     private String name;
 
     private String description;
 
-    private byte[] image;
-
+    @NotBlank
     private Tag tag;
 
     private SubTag subTag;
 
+    @Min(0)
+    @NotBlank
     private Integer cost;
 }
