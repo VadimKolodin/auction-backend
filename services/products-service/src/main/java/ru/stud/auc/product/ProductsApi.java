971e89a847a5e6ad28d0b8dff02f2fa3706e91ab
Вадим <105828231+VadimKolodin@users.noreply.github.com>
2023-03-29 00:20:36 +0400

    AUC-16 поиск по товарам (#8)
    * to test AUC-16
    
    * AUC-16 фикс мержа
    
    * AUC-16 fix
    
    * AUC-16 fix
    
    * AUC-16 fix
    
    * AUC-16 fix
    
    ---------
    
    Co-authored-by: Max <Tru.coder@yandex.ru>

--- services/products-service/src/main/java/ru/stud/auc/product/ProductsApi.java
+++ services/products-service/src/main/java/ru/stud/auc/product/ProductsApi.java
@@ -3,12 +3,17 @@ package ru.stud.auc.product;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.stud.auc.common.enums.SubTag;
 import ru.stud.auc.flowdata.product.model.ProductView;
 
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.UUID;
+
+import java.util.List;
+import java.util.Optional;
 
 import static ru.stud.auc.product.ProductsApi.PRODUCTS_API_PATH;
 
@@ -21,7 +26,7 @@ import static ru.stud.auc.product.ProductsApi.PRODUCTS_API_PATH;
 @RequestMapping(PRODUCTS_API_PATH)
 @Tag(name = "Методы для работы с товарами", description = PRODUCTS_API_PATH)
 public interface ProductsApi {
-    String PRODUCTS_API_PATH = "/api/product";
+    String PRODUCTS_API_PATH = "/api/products";
 
     @SecurityRequirements
     @GetMapping("/{productId}")
@@ -29,4 +34,16 @@ public interface ProductsApi {
     ProductView getProduct(@PathVariable UUID productId);
 
 
+    @GetMapping
+    @SecurityRequirements
+    @Operation(summary = "Поиск товаров по параметрам")
+    List<ProductView> searchProducts(
+            @RequestParam(name = "size", required = true)  @Min(1) @Max(100) int size,
+            @RequestParam(name = "page", required = true) @Min(0) int page,
+            @RequestParam(name = "nameSearchString") Optional<String> nameSearchString,
+            @RequestParam(name = "nameAsc")   Optional<Boolean> nameAsc,
+            @RequestParam(name = "costAsc")  Optional<Boolean> costAsc,
+            @RequestParam(name = "tags", required = false)   List<ru.stud.auc.common.enums.Tag> tags,
+            @RequestParam(name = "subTags", required = false)  List<SubTag> subTags
+    );
 }
--- services/products-service/src/main/java/ru/stud/auc/product/ProductsService.java
+++ services/products-service/src/main/java/ru/stud/auc/product/ProductsService.java
@@ -2,10 +2,15 @@ package ru.stud.auc.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.stud.auc.common.enums.SubTag;
+import ru.stud.auc.common.enums.Tag;
+import ru.stud.auc.exception.BadRequestException;
 import ru.stud.auc.flowdata.product.model.ProductView;
 import ru.stud.auc.product.model.ProductDto;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,5 +31,20 @@ public class ProductsService {
 
     public void restoreProduct(UUID productId) {
         productsUpdater.restoreProduct(productId);
+    }
+    @Transactional
+    public List<ProductView> searchProducts(int size,
+                                            int page,
+                                            Optional<String> nameSearchString,
+                                            Optional<Boolean> nameAsc,
+                                            Optional<Boolean> costAsc,
+                                            List<Tag> tags,
+                                            List<SubTag> subTags) {
+
+        if (nameAsc.isPresent() && costAsc.isPresent() && nameAsc.get() && costAsc.get()) {
+            throw new BadRequestException("Не могут быть одновременно выбраны 2 параметра сортировки. NameAsc = true && CostAsc = true");
+        }
+        int offset = page * size;
+        return productsInserter.searchProducts(size, offset, nameSearchString, nameAsc, costAsc, tags, subTags);
     }
 }
